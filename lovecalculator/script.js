c62ac37202fae78829ec1dcd87c962d21a69f6fa
Dzenana <dzenanamehmedovic8@gmail.com>
2022-10-23 03:54:35 -0700

    changes on lc

---
+++ lovecalculator/script.js
@@ -0,0 +1,16 @@
+document.getElementById("calculate").addEventListener("click", function () {
+    var randomNumber = Math.random() * 101;
+    var intNumber = Math.floor(randomNumber);
+  
+
+  
+    document.getElementById("result").textContent = intNumber + "%";
+    if (randomNumber < 40) {
+        document.getElementById("img").src =
+        "broken-heart.png"
+          
+      } else if (randomNumber > 50) {
+        document.getElementById("img").src =
+          "affection.png";
+      } 
+    });
