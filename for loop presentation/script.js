2234d5a9f2116d71508d1e4b6271665208afbd4b
Dzenana <dzenanamehmedovic8@gmail.com>
2022-10-28 05:56:26 -0700

    with explain

--- for loop presentation/script.js
+++ for loop presentation/script.js
@@ -36,9 +36,10 @@ document.getElementById("so").addEventListener("click", function () {
   
 });
 */
-
-var ocene= [];
+///
+var ocene= [];                             /*deklarisemo(inicijaliziramo niz()*/
 var suma=0;
+var k=0;
 
 
 
@@ -62,19 +63,33 @@ function pushNumber() {
   });
 
   document.getElementById("so").addEventListener("click", function () { 
+    
+          
     for (var i = 0; i< ocene.length; i++) { suma+=ocene[i]}
     console.log( "Srednja ocena iznosi :" + suma/ocene.length);
     console.log("Ukupno ocena " + ocene.length)
     console.log("Prva ocena koju je dobio ucenik je: " + ocene[0])
     
-
-        
-        
+    if(k<=ocene){
+        console.log('Ucenik je dobio nedovoljnu ocenu'+ k);k++
+    }
+});
+document.getElementById("showgrades").addEventListener("click", function () {
+document.getElementById("showgrades").textContent = ocene;
+       
     
 
-    });
+});
+ ////
+ 
+ 
+ 
+ //////////////////////////////////////////////////////////objasnjenja osnovnih pojmova    
+ /*for ([početna vrednost]; [uslov]; [izraz za povećenje]){
+  izraz za izvršenje}*/
 
-    document.getElementById("showgrades").addEventListener("click", function () {
-    document.getElementById("showgrades").textContent = ocene;});
-   
-    
+/*Naziv promenljve je tzv. "identifikatior" i sastoji se od velikih i malih slova engleske abecede, cifara, donje crte (_) i znaka za dolar ($), s tim što ne sme počinjati cifrom. 
+Identifikator (u novijim verzijama JavaScripta) može sadržati i slova nacionalnih pisama (npr. š,đ,č...)*/
+ /*Niz je složena promenljiva koja sadrži više drugih promenljivih u sebi.
+  Svaka promenljiva u nizu ima svoj redni broj (ili indeks, kako ćemo ga zvati u nastavku), pomoću kog se može naći vrednost koja nam treba.*/
+/*ako iza imena niza stavimo .length dobićemo koliko ima elemenata u nizu (npr. br.length predstavlja broj elemenata u nizu br). */
